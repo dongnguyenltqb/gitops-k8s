33b776948acfd66c135c0161ebe91bf7ff23a1b6
dongnguyenltqb <nhnguhoc@gmail.com>
2023-02-22 23:25:02 +0700

    add example vault ha

---
+++ vault-server/vault-ha/create-vault-table.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE vault_kv_store (
+        parent_path TEXT COLLATE "C" NOT NULL,
+        path        TEXT COLLATE "C",
+        key         TEXT COLLATE "C",
+        value       BYTEA,
+        CONSTRAINT pkey PRIMARY KEY (path, key)
+    );
+
+    CREATE INDEX parent_path_idx ON vault_kv_store (parent_path);
+
+    CREATE TABLE vault_ha_locks (
+        ha_key                                      TEXT COLLATE "C" NOT NULL,
+        ha_identity                                 TEXT COLLATE "C" NOT NULL,
+        ha_value                                    TEXT COLLATE "C",
+        valid_until                                 TIMESTAMP WITH TIME ZONE NOT NULL,
+        CONSTRAINT ha_key PRIMARY KEY (ha_key)
+    );
+
+EOSQL
